349583934155646cc970d1f653d078037ef62619
LOKESH SINGH <lokesh1129ece@gmail.com>
2024-04-08 04:16:58 +0530

    Added a lt section

--- components/MobileDashboard.tsx
+++ components/MobileDashboard.tsx
@@ -7,7 +7,7 @@ export default function MobileDashboard() {
       <div className="mt-4 flex  gap-12 max-sm:gap-8 bg-muted py-2 px-8 rounded-2xl max-w-full ">
           <DashboardLink href="/dashboard/timetable"><FilePieChart /></DashboardLink>
           <DashboardLink href="/dashboard/chat"><TextSearch /></DashboardLink>
-          <DashboardLink href="/dashboard/createuser">
+          <DashboardLink href="/dashboard/leacturetheater">
             <UserPlus />
           </DashboardLink>
           
